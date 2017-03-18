eb611e855fa46860484a85c2df595eaa0cd40cb2
Reza Agahi <reza.agahi.88@gmail.com>
2017-03-18 13:22:30 +0330

    add rate limiting and busy html page

--- routes/process.js
+++ routes/process.js
@@ -4,6 +4,21 @@ var ffi = require('ffi');
 var multer  = require('multer');
 var upload = multer({ dest: './public/images/uploads' });
 
+var RateLimit = require('ratelimit.js').RateLimit;
+var ExpressMiddleware = require('ratelimit.js').ExpressMiddleware;
+var redis = require('redis');
+
+var rateLimiter = new RateLimit(redis.createClient(), [{interval: 1, limit: 8}]);
+
+var options = {
+  ignoreRedisErrors: true // defaults to false
+};
+var limitMiddleware = new ExpressMiddleware(rateLimiter, options);
+var limitEndpoint = limitMiddleware.middleware(function(req, res, next) {
+  // res.status(429).json({message: 'rate limit exceeded'});
+  res.render('busy', {});
+});
+
 var processImage = ffi.Library('./build/libface', {
   'amooNowroozMaker': [ 'void', [ 'string', 'string' ] ],
   'init':['void', []]
@@ -22,7 +37,7 @@ router.get('/:id', upload.single('image'), function(req, res, next) {
   res.render('image', { imageUrl: '/images/processed/' + req.params.id + '.jpg'});
 });
 
-router.get('/', upload.single('image'), function(req, res, next) { 
+router.get('/', upload.single('image'), limitEndpoint, function(req, res, next) {
   processImage.amooNowroozMaker('./public/images/uploads/' +  'd26f08d255012f051582567086adc509' ,'./public/images/processed/' + 'd26f08d255012f051582567086adc509' + '.jpg');
   res.render('image', { imageUrl: '/images/processed/' + 'd26f08d255012f051582567086adc509' + '.jpg'});
 });
