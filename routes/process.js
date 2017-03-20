f43c5f51c59cfe1ae8ec1ee7c215149bf15cf242
Reza Agahi <reza.agahi.88@gmail.com>
2017-03-20 12:03:05 +0330

    change multer settings for input image validation

--- routes/process.js
+++ routes/process.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 var ffi = require('ffi');
 var multer  = require('multer');
-var upload = multer({ dest: './public/images/uploads' });
+var upload = multer({ dest: './public/images/uploads',
+                      fileFilter: function (req, file, cb) {
+                        if (file.mimetype !== 'image/jpeg' && file.originalname !== 'blob' && file.fieldname !== 'image') {
+                          return cb(new Error('Only image files are allowed!') ,false);
+                        }
+                        cb(null, true);
+                      },
+                      limits: { fileSize: 1 * 1000 * 1000 }
+                    });
 
 var RateLimit = require('ratelimit.js').RateLimit;
 var ExpressMiddleware = require('ratelimit.js').ExpressMiddleware;
@@ -32,7 +40,7 @@ router.post('/', upload.single('image'), limitEndpoint, function(req, res, next)
   res.json({id: req.file.filename});
 });
 
-router.get('/:id', upload.single('image'), function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   res.render('image', { imageUrl: '/images/processed/' + req.params.id + '.jpg',
                         message: 'تاری عکس، نور بد، فاصله زیاد یا دلایل دیگه ممکن است باعث عدم تشخیص چهره شود، در صورت بروز مجدد تلاش کنین.',
                         pageTopMessage: 'می تونی کارت تبریکت رو دانلود کنی یا برای کسایی که دوستشون داری تو تلگرام بفرستی. اگه کارت تبریکت رو دوست نداشتی می تونی برگردی و دوباره امتحان کنی.',
